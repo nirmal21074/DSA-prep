c810f2ef0c5bdfc250ffeed959da102c966db802
Nirmal Soni <97107556+nirmal21074@users.noreply.github.com>
2023-05-19 22:45:16 +0530

    Add files via upload

---
+++ CF probs/Team.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+    int n,a,b,c;
+    int count = 0;
+    cin >> n;
+    while (n--)
+    {
+        cin >>a>>b>>c;
+        if (a+b+c >=2)
+        {
+            count++;
+        }
+    }
+    cout<<count<<endl;
+    return 0;
+}
