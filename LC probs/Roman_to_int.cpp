205e8fe3dbeaf10fd8914c1f8d6c5cb3a1d549b3
Nirmal Soni <97107556+nirmal21074@users.noreply.github.com>
2023-06-01 22:32:40 +0530

    Add files via upload

---
+++ LC probs/Roman_to_int.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        map<char,int> roman = {{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+        int n = s.size();
+        int total = roman[s.back()];
+
+        for(int i = n-2; i>=0;i--){
+            if(roman[s[i]]<roman[s[i+1]]){
+                total-=roman[s[i]];
+            }
+            else{
+                total+=roman[s[i]];
+            }
+
+        }
+        return total;
+        
+    }
+};
